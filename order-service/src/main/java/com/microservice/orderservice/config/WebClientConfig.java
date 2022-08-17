8b3f1ecb9a3a4ac86333788f6a6834f270fda086
Joao Lopes (CTW) <Joao.CA.Lopes@ctw.bmwgroup.com>
2022-08-17 13:48:39 +0100

    Created discovery service

--- order-service/src/main/java/com/microservice/orderservice/config/WebClientConfig.java
+++ order-service/src/main/java/com/microservice/orderservice/config/WebClientConfig.java
@@ -1,15 +1,21 @@
 package com.microservice.orderservice.config;
 
+import io.netty.resolver.DefaultAddressResolverGroup;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
-    public WebClient webClient(){
-        return WebClient.builder().build();
+    @LoadBalanced //Client Side Load Balancer
+    public WebClient.Builder webClientBuilder(){
+        var httpClient = HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
+        return WebClient.builder().clientConnector(new ReactorClientHttpConnector(httpClient));
     }
-
 }
--- product-service/src/main/java/com/microservice/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/microservice/productservice/ProductServiceApplication.java
@@ -2,10 +2,12 @@ package com.microservice.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories
+@EnableEurekaClient
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
