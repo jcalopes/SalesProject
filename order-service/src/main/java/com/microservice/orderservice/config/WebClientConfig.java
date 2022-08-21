5596a657dc0f3adc631f19759b70ee7edd33f042
Joao Lopes (CTW) <Joao.CA.Lopes@ctw.bmwgroup.com>
2022-08-21 10:06:29 +0100

    Created API Gateway

--- order-service/src/main/java/com/microservice/orderservice/config/WebClientConfig.java
+++ order-service/src/main/java/com/microservice/orderservice/config/WebClientConfig.java
@@ -1,4 +1,5 @@
 package com.microservice.orderservice.config;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,7 +9,7 @@ import org.springframework.web.client.RestTemplate;
 public class WebClientConfig {
 
     @Bean
-    //@LoadBalanced //Client Side Load Balancer
+    @LoadBalanced //Client Side Load Balancer
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
