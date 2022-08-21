f9b16b50b20604eacec95906a1ef43d694aee200
Joao Lopes (CTW) <Joao.CA.Lopes@ctw.bmwgroup.com>
2022-08-21 21:25:00 +0100

    Updated circuit breaker to handle with timeOut responses and retry mechanisms

--- order-service/src/main/java/com/microservice/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/microservice/orderservice/controller/OrderController.java
@@ -4,12 +4,15 @@ import com.microservice.orderservice.dto.OrderDto;
 import com.microservice.orderservice.dto.OrderRequest;
 import com.microservice.orderservice.service.OrderService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,9 +23,11 @@ public class OrderController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
-    public String placeOrder(@RequestBody OrderRequest orderRequest){
+    @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
+    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest){
         log.info("POST: Called Order Service.");
-        return orderService.placeOrder(orderRequest);
+        return CompletableFuture.supplyAsync(()-> orderService.placeOrder(orderRequest));
     }
 
     @GetMapping
@@ -32,7 +37,7 @@ public class OrderController {
         return orderService.getOrders();
     }
 
-    public String fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
-        return "Something went error placing your order. Try again later.";
+    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
+        return CompletableFuture.supplyAsync(()-> "Something went error placing your order. Try again later.");
     }
 }
