4fb3aab7d7ee20ae29feb5f06a19dcf71817a58a
Joao Lopes (CTW) <joao.ca.lopes@ctw.bmwgroup.com>
2022-09-07 23:15:45 +0100

    Updated order service

--- order-service/src/main/java/com/microservice/orderservice/config/WebClientConfig.java
+++ order-service/src/main/java/com/microservice/orderservice/config/WebClientConfig.java
@@ -1,5 +1,4 @@
 package com.microservice.orderservice.config;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
